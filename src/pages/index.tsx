cde0665bd53495221da6f920d341218b60d2e797
dev <dev786.1@gmail.com>
2023-05-07 13:55:18 +0530

    three.js implementation in grogress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,26 @@ import Button from "../../components/atoms/button";
 import Footer from "../../components/organisms/footer";
 import Banner from "../../components/molecules/banner";
 
+
+import { useState } from "react";
+import ArrayVisualization from "../../components/organisms/gamePage";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: session } = useSession();
+  const [dataV, setDataV] = useState([2, 4, 6, 8]);
 
+  const handleClick = () => {
+    setDataV(dataV.map(value => value + 1));
+  };
   return (
     <>
       <Header title={""} />
-      <Banner/>
+      {/* <Banner imgSrc={""}/> */}
+      <ArrayVisualization/>
+      <button onClick={handleClick}>Increment Data</button>
       <div className="h-screen">
         <p>
           {session && session?.user?.email
