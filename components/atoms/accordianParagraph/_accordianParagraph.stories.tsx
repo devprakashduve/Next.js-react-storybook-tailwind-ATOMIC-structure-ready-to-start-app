19816306ffb26ae8d308327ca86245bde208d026
dev <dev786.1@gmail.com>
2023-07-02 13:27:00 +0530

    implementing bubble sorting

--- components/atoms/accordianParagraph/_accordianParagraph.stories.tsx
+++ components/atoms/accordianParagraph/_accordianParagraph.stories.tsx
@@ -11,7 +11,7 @@ const meta: Meta<typeof AccordianParagraph> = {
 export default meta;
 
 type Story = StoryObj<typeof AccordianParagraph>;
-const Template: Story = (args: AccordianParaProps) => (
+const Template: any = (args: AccordianParaProps) => (
   <AccordianParagraph {...args} />
 );
 
@@ -19,6 +19,7 @@ export const Primary: Story = Template.bind({});
 
 Primary.args = {
   title: "What is Flowbite?",
+  isOpen:true,
   content: ` Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
     Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.`,
 };
